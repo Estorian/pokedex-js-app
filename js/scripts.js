61977412d16120ec2567264524ae45e065b997af
Jason Lee <jason.lee.cultrans@gmail.com>
2020-10-08 10:33:05 -0600

    Update for Exercise 1.2
    This is the beginning of the pokedex list. It is designed to practice arrays, objects, and simple variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'Ice Cream';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']};
+pokemonList[1] = {name: 'Ivysaur', height:1, types:['grass', 'poison']};
+pokemonList[2] = {name: 'Venusaur', height:2, types:['grass', 'poison']};
+pokemonList[3] = {name: 'Charamander', height:0.6, types:['fire']};
