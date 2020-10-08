8f7bda0062075c265aacbfd8920d2f12adb35f64
Jason Lee <jason.lee.cultrans@gmail.com>
2020-10-08 15:38:38 -0600

    Restructured list and added printout
    I restructured the list within the js file to be easier to read. Then, as per Exercise 1.3 I added a printout that iterates over the array while checking for heights over 1m.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-let pokemonList = [];
-pokemonList[0] = {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']};
-pokemonList[1] = {name: 'Ivysaur', height:1, types:['grass', 'poison']};
-pokemonList[2] = {name: 'Venusaur', height:2, types:['grass', 'poison']};
-pokemonList[3] = {name: 'Charamander', height:0.6, types:['fire']};
+let pokemonList = [
+  {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
+  {name: 'Ivysaur', height:1, types:['grass', 'poison']},
+  {name: 'Venusaur', height:2, types:['grass', 'poison']},
+  {name: 'Charamander', height:0.6, types:['fire']},
+  {name: 'Charmeleon', height:1.1, types:['fire']},
+  {name: 'Charizard', height:1.7, types:['fire', 'flying']},
+  {name: 'Squirtle', height:0.5, types:['water']},
+  {name: 'Wartortle', height:1, types:['water']},
+  {name: 'Blastoise', height:1.6, types:['water']}
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  message = pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)';
+//Check for height over 1m and add to message if it is.
+  if (pokemonList[i].height > 1) {
+    message = message + ' - That\'s a big boy!';
+  }
+  message = message + '<br>' //Adds a line break after each entry.
+  document.write(message);
+}
