5017365e3bb5c74af001b9e6ff78436156abff1a
Jason Lee <jason.lee.cultrans@gmail.com>
2020-10-13 10:16:16 -0600

    Refactored using forEach loop
    As per Exercise 1.5.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+let pokemonRepository = (function () {
+  let pokemonList = [];
+
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  }
+})();
+
+
 let pokemonList = [
   {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
   {name: 'Ivysaur', height:1, types:['grass', 'poison']},
@@ -10,7 +24,16 @@ let pokemonList = [
   {name: 'Blastoise', height:1.6, types:['water']}
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+pokemonList.forEach(function(pokemon) {
+  message = '<p>' + pokemon.name + ' (height: ' + pokemon.height + 'm)';
+  if (pokemon.height > 1) {
+    message += ' That\'s a big boy!';
+  }
+  message += '</p>';
+  document.write(message);
+})
+
+/*for (let i = 0; i < pokemonList.length; i++) {
   message = pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)';
 //Check for height over 1m and add to message if it is.
   if (pokemonList[i].height > 1) {
@@ -19,3 +42,17 @@ for (let i = 0; i < pokemonList.length; i++) {
   message = message + '<br>' //Adds a line break after each entry.
   document.write(message);
 }
+*/
+function divide(dividend, divisor) {
+  if (divisor == 0) {
+    return "Dividing by zero will break the universe. Don't do that.";
+  }
+  else {
+    return (dividend / divisor);
+  }
+}
+
+console.log(divide(4, 2));
+console.log(divide(7, 0));
+console.log(divide(1, 4));
+console.log(divide(12, -3));
