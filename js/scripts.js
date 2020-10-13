e320ebbc4e4bd6b136208701b01a64c44209ebfe
Jason Lee <jason.lee.cultrans@gmail.com>
2020-10-13 11:07:26 -0600

    IIFE and lookUp function
    Changed code to put the pokemonList into an IIFE. Added add(pokemon), getAll(), and lookUp(queryName) functions to the IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
 let pokemonRepository = (function () {
-  let pokemonList = [];
+  let pokemonList = [
+    {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
+    {name: 'Ivysaur', height:1, types:['grass', 'poison']},
+    {name: 'Venusaur', height:2, types:['grass', 'poison']},
+    {name: 'Charamander', height:0.6, types:['fire']},
+    {name: 'Charmeleon', height:1.1, types:['fire']},
+    {name: 'Charizard', height:1.7, types:['fire', 'flying']},
+    {name: 'Squirtle', height:0.5, types:['water']},
+    {name: 'Wartortle', height:1, types:['water']},
+    {name: 'Blastoise', height:1.6, types:['water']}
+  ];
+
+  function add(pokemon) {
+    if (typeof(pokemon) === 'object') {
+      pokemonList.push(pokemon);
+    }
+    else {console.error('Invalid attempt to add non-object to pokemonList')};
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function lookUp(queryName) {
+    let pokeMatch = pokemonList.filter(pokemon => pokemon.name === queryName);
+    return pokeMatch;
+    }
 
   return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
-      return pokemonList;
-    }
-  }
+    add: add,
+    getAll: getAll,
+    lookUp: lookUp
+  };
 })();
 
-
-let pokemonList = [
-  {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
-  {name: 'Ivysaur', height:1, types:['grass', 'poison']},
-  {name: 'Venusaur', height:2, types:['grass', 'poison']},
-  {name: 'Charamander', height:0.6, types:['fire']},
-  {name: 'Charmeleon', height:1.1, types:['fire']},
-  {name: 'Charizard', height:1.7, types:['fire', 'flying']},
-  {name: 'Squirtle', height:0.5, types:['water']},
-  {name: 'Wartortle', height:1, types:['water']},
-  {name: 'Blastoise', height:1.6, types:['water']}
-];
-
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   message = '<p>' + pokemon.name + ' (height: ' + pokemon.height + 'm)';
   if (pokemon.height > 1) {
     message += ' That\'s a big boy!';
@@ -33,26 +43,4 @@ pokemonList.forEach(function(pokemon) {
   document.write(message);
 })
 
-/*for (let i = 0; i < pokemonList.length; i++) {
-  message = pokemonList[i].name + ' (height: ' + pokemonList[i].height + 'm)';
-//Check for height over 1m and add to message if it is.
-  if (pokemonList[i].height > 1) {
-    message = message + ' - That\'s a big boy!';
-  }
-  message = message + '<br>' //Adds a line break after each entry.
-  document.write(message);
-}
-*/
-function divide(dividend, divisor) {
-  if (divisor == 0) {
-    return "Dividing by zero will break the universe. Don't do that.";
-  }
-  else {
-    return (dividend / divisor);
-  }
-}
-
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
+console.log(pokemonRepository.lookUp("Squirtle"));
