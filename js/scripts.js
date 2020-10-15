e2e45e691e2a03839acd59e9f59c7e9e4d684179
Jason Lee <jason.lee.cultrans@gmail.com>
2020-10-15 09:52:56 -0600

    Implemented pokeapi
    Added fetch  (w/polyfill) and promises to load list from pokeapi. Added functions to PokemonRepository to load the list and populate the pokemon details.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Bulbasaur', height:0.7, types:['grass', 'poison']},
-    {name: 'Ivysaur', height:1, types:['grass', 'poison']},
-    {name: 'Venusaur', height:2, types:['grass', 'poison']},
-    {name: 'Charamander', height:0.6, types:['fire']},
-    {name: 'Charmeleon', height:1.1, types:['fire']},
-    {name: 'Charizard', height:1.7, types:['fire', 'flying']},
-    {name: 'Squirtle', height:0.5, types:['water']},
-    {name: 'Wartortle', height:1, types:['water']},
-    {name: 'Blastoise', height:1.6, types:['water']}
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+//checks for an object, then pushes the pokemon into pokemonList.
   function add(pokemon) {
     if (typeof(pokemon) === 'object') {
       pokemonList.push(pokemon);
@@ -18,15 +10,18 @@ let pokemonRepository = (function () {
     else {console.error('Invalid attempt to add non-object to pokemonList')};
   }
 
+//returns pokemonList
   function getAll() {
     return pokemonList;
   }
 
+//searches for a specific pokemon by name and returns that pokemon object.
   function lookUp(queryName) {
     let pokeMatch = pokemonList.filter(pokemon => pokemon.name === queryName);
     return pokeMatch;
   }
 
+//adds a new pokemon in the list and creates a button for it.
   function addListItem(pokemon) {
     let list = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
@@ -34,32 +29,70 @@ let pokemonRepository = (function () {
     button.innerText = pokemon.name;
     button.classList.add('pokeName');
     detailsButtonListener(button, pokemon);
-//    button.addEventListener('click', function (event) {showDetails(pokemon);});
     listItem.appendChild(button);
     list.appendChild(listItem);
   }
 
+//Setup the event listener on the button using showDetails()
   function detailsButtonListener(button, pokemon) {
     button.addEventListener('click', function (event) {
       showDetails(pokemon);
     })
   }
 
+//takes a pokemon as a parameter and displays its information
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+//Loads the list of 150 pokemon from pokeapi.
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
   }
 
   return {
     add: add,
     getAll: getAll,
     lookUp: lookUp,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
+
 
 
 console.log(pokemonRepository.lookUp("Squirtle"));
